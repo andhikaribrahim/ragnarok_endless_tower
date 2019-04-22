df2ec958851dd27f6358333f09901db182f5fe45
Andhika R Ibrahim <andhikaribrahim@gmail.com>
2019-04-22 18:32:43 +0700

    Add TODO

--- crawler.rb
+++ crawler.rb
@@ -2,6 +2,9 @@ require "httparty"
 require "nokogiri"
 require "pry"
 
+# TODO:
+# Set prompt or enable initializers params to be set during command line process
+
 class Crawler
   def initialize(server: nil)
     @date = Time.now.to_i
@@ -16,7 +19,25 @@ class Crawler
    @parsed = Nokogiri::HTML(HTTParty.post(@url, :query => { server: @server }))
   end
 
+  def get_data_needed
+    # TODO: get data needed
+  end
 
+  def get_mvp_data
+    # TODO: get MVP data
+  end
+
+  def get_smvp_data
+    # TODO: get S-MVP data
+  end
+
+  def simulate_tabs_click
+    # TODO: simulate tabs click to fetch S-MVP data
+  end
+
+  def convert_data_as_file
+    # TODO: convert collected data to JSON file
+  end
 end
 
 Crawler.new
